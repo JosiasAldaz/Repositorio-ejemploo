c2a1fa833414f8dd452e3515d7894fcfa3c9c706
JosiasAldaz <josiasjoestar@hotmail.com>
2024-05-30 15:45:33 -0500

    se a subido la actualización de estudiante

---
+++ src/java/test/TestWS.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class TestWS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // CREAR EL CLIENTE
+        //WSO servicio = new WSOperaciones_Service();
+    }
+    
+}
